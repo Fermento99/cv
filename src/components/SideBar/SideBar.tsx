ed01d565d884fe07472c401be9baf85070a91d4c
Paweł Kajanek <pawel.kajanek@stxnext.pl>
2025-04-29 14:56:47 +0200

    add summary

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -3,9 +3,9 @@ import Skills from "@/components/SideBar/Skills";
 
 export default function SideBar() {
   return (
-    <aside className="w-1/4 bg-background-sidePanel px-4 pb-8 pt-16 flex flex-col h-vh">
+    <aside className="w-1/4 bg-background-sidePanel px-8 pb-8 pt-16 flex flex-col h-vh print:px-4">
       <Contact />
-      <hr className="border-separator-light -mx-2" />
+      <hr className="border-separator-light -mx-4 print:-mx-2" />
       <Skills />
     </aside>
   );
